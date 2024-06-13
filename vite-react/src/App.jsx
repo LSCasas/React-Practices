22c90cf37a76535b822a5c2cfa4e099ad7515b0e
Luis Casas <luisscasas45@gmail.com>
2024-06-12 19:15:24 -0600

    FEAT/ corretion of name

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Menu from "./components/Menu";
+
+
+export default function App() {
+  return (
+    <main id="main-layout">
+      <NavBar />
+      <Menu />
+      <Content />
+    </main>
+  );
+}
